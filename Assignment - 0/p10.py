a1e2cb1f84af59f17b3c964a44bae2bb136ce15b
Yatharth Sharma <syatharth209@gmail.com>
2026-07-17 15:28:31 +0530

    Create p10.py

---
+++ Assignment - 0/p10.py
@@ -0,0 +1,6 @@
+original_list = [1, 2, 2, 3, 4, 4, 4, 5, 1, 6]
+
+unique_list = list(set(original_list))
+
+print("Original List:", original_list)
+print("List after removing duplicates:", unique_list)

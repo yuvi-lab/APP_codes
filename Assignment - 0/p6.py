e8e5becec7a6886e0546a2a19ad2de2e5ddde8cc
Yatharth Sharma <syatharth209@gmail.com>
2026-07-17 15:26:02 +0530

    Create p6.py

---
+++ Assignment - 0/p6.py
@@ -0,0 +1,16 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+
+if num1 >= num2:
+    if num1 >= num3:
+        greatest = num1
+    else:
+        greatest = num3
+else:
+    if num2 >= num3:
+        greatest = num2
+    else:
+        greatest = num3
+
+print(f"The greatest number among {num1}, {num2}, and {num3} is: {greatest}")
